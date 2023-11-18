980e292a80ff09fd3bf19058c85ff4b60f678a9c
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-11-18 17:57:46 +0100

    create sql function

---
+++ exec_sql.py
@@ -0,0 +1,30 @@
+from sql_functions import *
+
+conn = create_connection()
+execute_sql(conn, create_books_sql)
+execute_sql(conn, create_authors_sql)
+
+delete_all_data(conn, "books")
+delete_all_data(conn, "authors")
+
+insert_data(conn, "books", title="test",
+            author_id=1, isbn="1234567890")
+insert_data(conn, "books", title="Krzyzacy",
+            author_id=2, isbn="9876512340")
+insert_data(conn, "books", title="Przedwiosnie",
+            author_id=3, isbn="0987654321")
+
+insert_data(conn, "authors", first_name="Michal",
+            last_name="Kaczmarek")
+insert_data(conn, "authors", first_name="Henryk",
+            last_name="Sienkiewicz")
+insert_data(conn, "authors", first_name="Stefan",
+            last_name="Żeromski")
+
+update_data(conn, "books" , 2, author_id=3, title="test2")
+
+delete_data(conn, "books", 3)
+
+select_all_data(conn, "books")
+
+select_data_where(conn, "books", title="test", isbn="1234567890", author_id=1)
---
+++ sql_functions.py
@@ -0,0 +1,121 @@
+import sqlite3
+from sqlite3 import Error
+
+
+create_authors_sql = """
+    CREATE TABLE IF NOT EXISTS authors (
+        id integer PRIMARY KEY,
+        first_name varchar(50) NOT NULL,
+        last_name varchar(50) NOT NULL
+    );
+"""
+
+create_books_sql = """
+    CREATE TABLE IF NOT EXISTS books (
+        id integer PRIMARY KEY,
+        author_id integer NOT NULL,
+        title varchar(50) NOT NULL,
+        isbn varchar(15) NOT NULL,
+        FOREIGN KEY (author_id) REFERENCES authors (id) 
+    );
+"""
+
+
+def create_connection():
+    try:
+        conn = sqlite3.connect("library.db")
+        return conn
+    except Error as e:
+        print(e)
+
+
+def execute_sql(conn, sql):
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+
+def insert_data(conn, table, **kwargs):
+    try:
+        columns = [f"{k}" for k in kwargs]
+        columns = ", ".join(columns)
+        parameters = [f"?" for v in kwargs]
+        parameters = ", ".join(parameters)
+        values = tuple(v for v in kwargs.values())
+        sql = f"INSERT INTO {table}({columns}) VALUES({parameters})"
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+
+def update_data(conn, table, id, **kwargs):
+    parameters = [f"{k}=?" for k in kwargs]
+    parameters = ", ".join(parameters)
+    values = tuple(v for v in kwargs.values())
+    values += (id, )
+    sql = f''' UPDATE {table}
+                SET {parameters}
+                WHERE id = ?'''
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        print("OK")
+    except sqlite3.OperationalError as e:
+        print(e)
+
+
+def delete_data(conn, table, id):
+    values = (id, )
+    sql = f''' DELETE FROM {table}
+                WHERE id = ?'''
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        print("OK")
+    except sqlite3.OperationalError as e:
+        print(e)
+
+
+def delete_all_data(conn, table):
+    sql = f'DELETE FROM {table}'
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        print("OK")
+    except sqlite3.OperationalError as e:
+        print(e)
+
+
+def select_all_data(conn, table):
+    sql = f'SELECT * FROM {table}'
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        print(rows)
+        print("OK")
+    except Error as e:
+        print(e)
+
+
+def select_data_where(conn, table, **kwargs):
+    parameters = [f"{k}=?" for k in kwargs]
+    parameters = " AND ".join(parameters)
+    values = tuple(v for v in kwargs.values())
+    sql = f"SELECT * FROM {table} WHERE {parameters}"
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        rows = cur.fetchall()
+        print(rows)
+        print("OK")
+    except Error as e:
+        print(e)
