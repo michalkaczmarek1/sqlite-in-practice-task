2b65dfa9559f4ed9a6041dba13b810dbc25a6b95
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-11-19 23:26:06 +0100

    fix after review

--- exec_sql.py
+++ exec_sql.py
@@ -8,19 +8,19 @@ if __name__ == "__main__":
     sql_functions.delete_all_data(conn, "books")
     sql_functions.delete_all_data(conn, "authors")
 
-    sql_functions.insert_data(conn, "books", title="test",
-                              author_id=1, isbn="1234567890")
-    sql_functions.insert_data(conn, "books", title="Krzyzacy",
-                              author_id=2, isbn="9876512340")
-    sql_functions.insert_data(conn, "books", title="Przedwiosnie",
-                              author_id=3, isbn="0987654321")
-
     sql_functions.insert_data(conn, "authors", first_name="Michal",
                               last_name="Kaczmarek")
     sql_functions.insert_data(conn, "authors", first_name="Henryk",
                               last_name="Sienkiewicz")
     sql_functions.insert_data(conn, "authors", first_name="Stefan",
                               last_name="Żeromski")
+
+    sql_functions.insert_data(conn, "books", title="test",
+                              author_id=1, isbn="1234567890")
+    sql_functions.insert_data(conn, "books", title="Krzyzacy",
+                              author_id=2, isbn="9876512340")
+    sql_functions.insert_data(conn, "books", title="Przedwiosnie",
+                              author_id=3, isbn="0987654321")
 
     sql_functions.update_data(conn, "books", 2, author_id=3, title="test2")
 
